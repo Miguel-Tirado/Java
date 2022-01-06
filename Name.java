e0c3f4dec6940bbb097fd71d68ade937b4d5ef0d
Miguel <miguel.e.tirado11@gmail.com>
2022-01-05 20:30:44 -0800

    simple while loop name check

---
+++ Name.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Name {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+
+        while (!name.equals("miguel")) {
+            System.out.println("please enter the correct name: ");
+            name = sc.nextLine();
+        }
+
+        System.out.println("You entered the correct name!");
+
+
+    }
+}
