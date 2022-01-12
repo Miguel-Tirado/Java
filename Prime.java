02d0cf2ab5d5dfb5958d897339d504e25001e36e
Miguel <miguel.e.tirado11@gmail.com>
2022-01-11 19:07:12 -0800

    fixed prime class

--- Prime.java
+++ Prime.java
@@ -1,19 +1,25 @@
 public class Prime {
     public static void main(String[] args) {
         int low = 20, high = 50;
+        while (low < high) {
+            if(CheckPrime(low)) {
+                System.out.print(low + " ");
+            }
+            low++;
+        }
+        
 
     }
-
-    public static boolean CheckPrime(int lo, int hi) {
-        int total = hi - lo;
-        int prime = total;
-        for (int i = 0; i < total; i++) {
-            if (prime % 2 == 0) {
-                return false;
-            } else {
-                return true; 
+    
+    public static boolean CheckPrime(int num) {
+        boolean flag = true;
+        for(int i = 2; i < num/2; i++) { //i must be 2 since any num can be mod by 1 for the if statement below 
+            if(num % i == 0) {
+                flag = false; 
+                return flag;
             }
         }
+        return flag;
     }
 
 }
