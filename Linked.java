2796f8587eab5a6d2822a93b8b95d2b61ac882e7
Miguel <miguel.e.tirado11@gmail.com>
2022-01-11 19:17:03 -0800

    Created simple linked list

---
+++ Linked.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Linked {
+    public static void main(String[] args) {
+        LinkedList queue = new LinkedList();
+        queue.add(1);
+        queue.add(6);
+        System.out.print(queue);
+        
+    }
+}
