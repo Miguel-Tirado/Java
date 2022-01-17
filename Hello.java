b73f46902e5e612a8fdc1c35c7f7f14304bea203
Miguel <miguel.e.tirado11@gmail.com>
2022-01-16 20:02:48 -0800

    hello program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        int sum = x + y;
+        System.out.println(x + " + " + y  + " = " + sum) ; 
+    }
+}
